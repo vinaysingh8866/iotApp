178bc637b41d261472bb6d371f50b0fa2996861b
Vinay Singh <A00231503@mycambrian.ca>
2021-07-30 13:28:25 -0400

    Database Context And Devices Model Added

---
+++ iotAPI/Context/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using iotAPI.Models;
+
+namespace FirstASPNetCoreWebsite.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Devices> Devices { get; set; }
+    }
+}
---
+++ iotAPI/Models/Devices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iotAPI.Models
+{
+    public class Devices
+    {
+        [Key]
+        [HiddenInput]
+        public int ID { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string deviceType { get; set; }
+        [Required]
+        public string deviceDataUrl { get; set; }
+
+
+    }
+}
