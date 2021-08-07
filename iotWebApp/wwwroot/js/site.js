4fcb1c88b37530ead4e3adc271307fc65ac7230a
Vinay Singh <A00231503@mycambrian.ca>
2021-08-06 21:22:38 -0400

    FireBase Error Fixed

--- iotWebApp/wwwroot/js/site.js
+++ iotWebApp/wwwroot/js/site.js
@@ -1,5 +1,21 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDVN17857pJ67ktDKXKwBYrZbBU1r4lW-c",
+    authDomain: "iotapp-8edb8.firebaseapp.com",
+    databaseURL: "https://iotapp-8edb8-default-rtdb.firebaseio.com",
+    projectId: "iotapp-8edb8",
+    storageBucket: "iotapp-8edb8.appspot.com",
+    messagingSenderId: "658233283262",
+    appId: "1:658233283262:web:4447cc3d242c21f37184ef",
+    measurementId: "G-CWS0BX6DTM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 const deviceId = document.getElementById("deviceId")
 const deviceType = document.getElementById("deviceType")
 const deviceValue = document.getElementById("deviceValue")
